8da4a3200756561cf10b957d11bf693e4a266ee3
Norberto Santiago <norbertosantiago82@gmail.com>
2020-02-25 19:34:07 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,9 +3,9 @@ require 'pry'
 
 def create_project_hash
  html = File.read('fixtures/kickstarter.html')
- 
-kickstarter = Nokogiri::HTML(html)
+ kickstarter = Nokogiri::HTML(html)
  binding.pry
-end
+end 
+# projects: kickstarter.css("li.project.grid_4")
 
 create_project_hash
